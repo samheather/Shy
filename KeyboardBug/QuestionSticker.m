9f015eedae827f6a44997684f839a441dae9ff77
Sam Heather <sam@heather.sh>
2014-09-07 13:21:53 +0100

    First refactoring - now works.  Needs cleaning up.

--- KeyboardBug/QuestionSticker.m
+++ KeyboardBug/QuestionSticker.m
@@ -13,6 +13,9 @@
 
 int initialHeight = 150;
 CGRect originalStickerRect;
+UIImageView *sideBar;
+UILabel *questionTitle;
+UILabel *answer;
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -36,6 +39,12 @@ CGRect originalStickerRect;
         
         originalStickerRect = self.frame;
         
+        [self setBackgroundColor:[UIColor whiteColor]];
+        [self.layer setShadowColor:[UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:1].CGColor];
+        [self.layer setShadowOffset:CGSizeMake(0, 0)];
+        [self.layer setShadowOpacity:0.8];
+        [self.layer setShadowRadius:1];
+        
         // Add in corner radius, shadows, questions and answers.
         
         [self addTarget:self action:@selector(tapDown) forControlEvents:UIControlEventTouchDown];
@@ -44,6 +53,28 @@ CGRect originalStickerRect;
         [self addTarget:self action:@selector(tapUpOutside) forControlEvents:UIControlEventTouchDragOutside];
         // TODO - this means if dragging inside it's 'deselected' - is this expected/desired behaviour?
         [self addTarget:self action:@selector(tapUpOutside) forControlEvents:UIControlEventTouchDragInside];
+        
+        // TODO Finish this code to use the return sidebar gradient below and handle null for grey background.
+        // TODO check handling of null?  Since I don't use this specificaly now, sets grey and puts image on top.
+        sideBar = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,10,self.frame.size.height)];
+        [sideBar setBackgroundColor:[UIColor grayColor]];
+        [sideBar setImage:[self getSideBarImage:[[inputQuestion splitExternalKeywords] objectAtIndex:0]]];
+        [self addSubview:sideBar];
+        
+        questionTitle = [[UILabel alloc] initWithFrame:CGRectMake(20,10,self.frame.size.width-20,15)];
+        [questionTitle setText:[inputQuestion question]];
+        [self addSubview:questionTitle];
+        
+        answer = [[UILabel alloc] initWithFrame:CGRectMake(20, 35,self.frame.size.width-20,initialHeight-35-10)];
+        [answer setText:[inputQuestion answer]];
+        //        answer.contentVerticalAlignment = UIControlContentVerticalAlignmentTop;
+        [answer setNumberOfLines:4];
+        //        answer.
+        [self addSubview:answer];
+        
+        [self.layer setCornerRadius:4];
+        self.layer.masksToBounds = YES;
+
     }
     return self;
 }
@@ -59,6 +90,38 @@ CGRect originalStickerRect;
 -(void)tapUpOutside {
     NSLog(@"Tap up outside");
 }
+
+-(UIImage *)getSideBarImage:(NSString *)category {
+    if ([category isEqualToString:@"Drugs & Alcohol"]) {
+        return [UIImage imageNamed:@"drugsandalcohol-darkgreen"];
+    }
+    else if ([category isEqualToString:@"Family"]) {
+        return [UIImage imageNamed:@"family-orange"];
+    }
+    else if ([category isEqualToString:@"Feelings"]) {
+        return [UIImage imageNamed:@"feelings-deeppurple"];
+    }
+    else if ([category isEqualToString:@"Friends"]) {
+        return [UIImage imageNamed:@"friends-yellow"];
+    }
+    else if ([category isEqualToString:@"Health"]) {
+        return [UIImage imageNamed:@"health-green"];
+    }
+    else if ([category isEqualToString:@"Relationships"]) {
+        return [UIImage imageNamed:@"relationships-purple"];
+    }
+    else if ([category isEqualToString:@"School"]) {
+        return [UIImage imageNamed:@"school-turqouise"];
+    }
+    else if ([category isEqualToString:@"Sex"]) {
+        return [UIImage imageNamed:@"sex-redish"];
+    }
+    else if ([category isEqualToString:@"Your Body"]) {
+        return [UIImage imageNamed:@"yourbody-blue"];
+    }
+    return NULL;
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
